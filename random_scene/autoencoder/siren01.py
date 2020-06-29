3bfe6b055f64d76e130b173dd191aaf7e042e3ba
BuzzLord <buzzlord@gmail.com>
2020-06-28 21:31:48 -0600

    Removed batchnorm, fixed w0 in sin.
    Checked out siren-pytorch, and noted that I had these things wrong.
    No batchnorm (it was causing deeper networks to converge super slow, if
    at all). Changed the w0 factor to be in the sin activation, instead of
    just scaling the weight initially.
    
    Also added print-statistics, to see basic mean/stdev of weights for all
    layers, to see if/when they change.

--- random_scene/autoencoder/siren01.py
+++ random_scene/autoencoder/siren01.py
@@ -27,22 +27,12 @@ class ModelLoss(nn.Module):
 
 
 class Sine(nn.Module):
-    __constants__ = ['inplace']
-
-    def __init__(self, inplace=False):
+    def __init__(self, w0=1.):
         super(Sine, self).__init__()
-        self.inplace = inplace
+        self.w0 = w0
 
     def forward(self, x):
-        if self.inplace:
-            result = torch.sin_(x)
-        else:
-            result = torch.sin(x)
-        return result
-
-    def extra_repr(self):
-        inplace_str = 'inplace=True' if self.inplace else ''
-        return inplace_str
+        return torch.sin(self.w0 * x)
 
 
 class Siren(nn.Module):
@@ -63,70 +53,46 @@ class Siren(nn.Module):
         input_size = 2 * 5 * pos_encoding_levels
         output_size = 3
 
+        self.first_weight_scale = 30.0
         self.hidden_layers = hidden_layers
         layers = [nn.Sequential(OrderedDict([("linear", nn.Linear(input_size, hidden_size)),
-                                             ("sine", Sine(inplace=False)),
-                                             ("batchnorm", nn.BatchNorm1d(hidden_size))]))]
+                                             ("sine", Sine(w0=self.first_weight_scale))]))]
         for i in range(2, hidden_layers):
             layers.append(
                 nn.Sequential(
-                    OrderedDict([("linear".format(i), nn.Linear(hidden_size, hidden_size)),
-                                 ("sine".format(i), Sine(inplace=False)),
-                                 ("batchnorm".format(i), nn.BatchNorm1d(hidden_size))])
+                    OrderedDict([("linear", nn.Linear(hidden_size, hidden_size)),
+                                 ("sine", Sine())])
                 ))
         layers.append(nn.Sequential(
             OrderedDict([("linear", nn.Linear(hidden_size, output_size))])
         ))
         self.network = nn.Sequential(OrderedDict([("layer{:d}".format(i+1), layer) for i, layer in enumerate(layers)]))
 
-        #self.linear1 = nn.Linear(input_size, hidden_size)
-        #self.linear2 = nn.Linear(hidden_size, hidden_size)
-        #self.linear3 = nn.Linear(hidden_size, hidden_size)
-        #self.linear4 = nn.Linear(hidden_size, hidden_size)
-        #self.linear5 = nn.Linear(hidden_size, output_size)
-
-        #self.bn1 = nn.BatchNorm1d(hidden_size)
-        #self.bn2 = nn.BatchNorm1d(hidden_size)
-        #self.bn3 = nn.BatchNorm1d(hidden_size)
-        #self.bn4 = nn.BatchNorm1d(hidden_size)
-
-        self.first_weight_scale = 30.0
-
         # Initialize weights
         for k, m in self.named_modules():
             if isinstance(m, nn.Linear):
                 n = m.in_features
                 m.weight.data.uniform_(-math.sqrt(6/n), math.sqrt(6/n))
-                m.bias.data.uniform_(-math.sqrt(6/n), math.sqrt(6/n))
-                if k == "network.layer1.linear" or k == "linear1":
-                    m.weight.data.mul_(self.first_weight_scale)
-            elif isinstance(m, nn.BatchNorm1d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
+                m.bias.data.uniform_(-math.sqrt(1/n), math.sqrt(1/n))
 
     def forward(self, inputs):
         x = torch.cat([torch.cat((torch.sin(math.pow(2,i)*math.pi*inputs), torch.cos(math.pow(2,i)*math.pi*inputs)),
                                  dim=1) for i in range(self.pos_encoding_levels)], dim=1)
-
-        #for i in range(self.hidden_layers-1):
-        #    x = self.batch_norm[i](torch.sin(self.linear[i](x)))
-        #out = self.linear[-1](x)
-
-        #x = self.bn1(torch.sin(self.linear1(x)))
-        #x = self.bn2(torch.sin(self.linear2(x)))
-        #x = self.bn3(torch.sin(self.linear3(x)))
-        #x = self.bn4(torch.sin(self.linear4(x)))
-        #out = self.linear5(x)
-
         out = self.network(x)
-
         return out
+
+    def print_statistics(self):
+        for k, m in self.named_modules():
+            if isinstance(m, nn.Linear):
+                logging.info("{}: weights {:.6f} ({:.6f})".format(k, m.weight.data.mean(), m.weight.data.std()))
 
 
 def train(args, model, device, train_loader, criterion, optimizer, epoch):
     model.train()
     train_set = train_loader.dataset
     for image_idx, image_filename in enumerate(train_loader):
+        if args.print_statistics:
+            model.print_statistics()
         data_loaders = train_set.generate_dataloaders(image_filename)
 
         batch_idx = 0
@@ -189,7 +155,7 @@ def test(args, model, device, test_loader, criterion, epoch):
                 loss_value = "{:.3e} ({:.3e})".format(mean(test_loss), stdev(test_loss))
             else:
                 loss_value = "{:.3e}".format(test_loss[0])
-            logging.info('Test set ({:.0f}%) loss: {}'.format((image_idx+1)/len(test_loader), loss_value))
+            logging.info('Test set ({:.0f}%) loss: {}'.format(100. * (image_idx+1) / len(test_loader), loss_value))
 
             if image_idx % int(len(test_loader)/6) == 0:
                 saver_loader = dl.RandomSceneSirenSampleSet(join(test_set.root_dir, image_filename[0]),
@@ -215,9 +181,9 @@ def main(custom_args=None):
     # Training settings
     model_number = "01"
     parser = argparse.ArgumentParser(description='PyTorch SIREN Model ' + model_number + ' Experiment')
-    parser.add_argument('--batch-size', type=int, nargs='+', default=256, metavar='N',
-                        help='input batch size for training (default: 256)')
-    parser.add_argument('--test-batch-size', type=int, nargs='+', default=512, metavar='N',
+    parser.add_argument('--batch-size', type=int, default=512, metavar='N',
+                        help='input batch size for training (default: 512)')
+    parser.add_argument('--test-batch-size', type=int, default=512, metavar='N',
                         help='input batch size for testing (default: 512)')
     parser.add_argument('--epochs', type=int, default=20, metavar='N',
                         help='number of epochs to train (default: 20)')
@@ -256,6 +222,8 @@ def main(custom_args=None):
                         help='Hidden layer count of Siren (default: 5)')
     parser.add_argument('--pos-encoding', type=int, default=4, metavar='L',
                         help='Positional encoding harmonics (default: 4)')
+    parser.add_argument('--print-statistics', action='store_true', default=False,
+                        help='print out layer weight statistics periodically')
     args = parser.parse_args(args=custom_args)
 
     if args.model_path is None:
@@ -311,13 +279,13 @@ def get_data_loaders(args, kwargs):
     position_scale = [1 / 4, 1 / 3, 1 / 3]
     dataset_path = join('..', args.dataset)
     train_set = dl.RandomSceneSirenFileList(root_dir=dataset_path, dataset_seed=args.dataset_seed, is_test=False,
-                                            batch_size=512, num_workers=4, pin_memory=True, shuffle=True,
+                                            batch_size=args.batch_size, num_workers=4, pin_memory=True, shuffle=True,
                                             pos_scale=position_scale)
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=4, shuffle=True)
 
     test_set = dl.RandomSceneSirenFileList(root_dir=dataset_path, dataset_seed=args.dataset_seed, is_test=True,
-                                           batch_size=512, num_workers=4, pin_memory=False, shuffle=False,
-                                           pos_scale=position_scale)
+                                           batch_size=args.test_batch_size, num_workers=4, pin_memory=False,
+                                           shuffle=False, pos_scale=position_scale)
     test_loader = torch.utils.data.DataLoader(test_set, batch_size=4, shuffle=False)
 
     return test_loader, train_loader
